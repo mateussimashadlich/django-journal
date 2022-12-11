cd3bfb1f1c3ba0ba42366249df817b898d20cbff
Mateus Simas Hadlich <mateussimashadlich@gmail.com>
2022-12-10 22:03:04 -0300

    chore: add model migration and file upload path

--- backend/publications/models.py
+++ backend/publications/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Publication(models.Model):
-    thumbnail = models.ImageField('Thumbnail')
+    thumbnail = models.ImageField('Thumbnail', upload_to='thumbnails')
     titulo = models.CharField('Título', max_length=255)
     data = models.DateTimeField(auto_now_add=True)
     data_edicao = models.DateTimeField(auto_now=True)
